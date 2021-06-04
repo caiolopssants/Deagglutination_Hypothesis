24bb55f8d77d86eaa41340338364328165ccf4af
caio deodato santos <caiodeodato@hotmail.com>
2021-06-04 17:34:45 -0300

    Init

--- Deagglutination_Class/Deagglutination_Class.cs
+++ Deagglutination_Class/Deagglutination_Class.cs
@@ -30,7 +30,9 @@ namespace Deagglutination_Class
 
             string value = number.ToString();            
             uint a = Convert.ToUInt32(value.Substring(0, value.Length - 1));//converter os algarismo, até o penúltimo algarismo
-            uint b = Convert.ToUInt32(value[value.Length - 1].ToString());//converte o último algarismo                
+            uint b = Convert.ToUInt32(value[value.Length - 1].ToString());//converte o último algarismo     
+            uint initalA = a;//Consersvando argumento 'a' original
+            uint initialB = b;//Consersvando argumento 'b' original
             Tuple<bool, string> answer = null;
 
             if (b == 1)//caso b seja igual a 1, deverá ser incrementado 10 para evitar falso positivo, quando chamado o método IsPrimeNumber
@@ -43,24 +45,31 @@ namespace Deagglutination_Class
             else//caso conste como um possível número primo
             {
                 string newValue = string.Empty;
-                if (b != 1) //Caso 'b' seja diferente de 1, dessa forma, evita-se duplicação de uma chamada de método, com o mesmo argumento
+                if (initialB != 1)//verificação se o último algarismo inicial ('b') seja diferente de 1, dessa forma, evita-se duplicação de uma chamada de método, com o mesmo argumento
                 {
                     newValue = (Convert.ToUInt32(value) * b).ToString();//novo valor, multiplicado pelo último algarismo
                     a = Convert.ToUInt32(newValue.Substring(0, newValue.Length - 1));//converter os algarismo, até o penúltimo algarismo
                     b = Convert.ToUInt32(newValue[newValue.Length - 1].ToString());//converte o último algarismo
-                    answer = ApplyDeagglutination(a, b);//verifica se o valor é primo
+                    if (b == 1)//caso b seja igual a 1, deverá ser incrementado 10 para evitar falso positivo, quando chamado o método IsPrimeNumber
+                        answer = ApplyDeagglutination(a - 1, b + 10);//verifica se o valor é primo
+                    else//caso contrário
+                        answer = ApplyDeagglutination(a, b);//verifica se o valor é primo
                 }
 
                 if (!answer.Item1)//caso não seja primo
                     return answer;
                 else//caso conste como um possível número primo
-                {
-                    if (b == 9)//verificação se o último algarismo é o número 3
+                {                    
+                    if (initialB == 9)//verificação se o último algarismo inicial é o número 3
                     {
-                        newValue = (Convert.ToUInt32(value) * 3).ToString();//novo valor, multiplicado por 7
+                        newValue = (Convert.ToUInt32(value) * 3).ToString();//novo valor, multiplicado por 3
                         a = Convert.ToUInt32(newValue.Substring(0, newValue.Length - 1));//converter os algarismo, até o penúltimo algarismo
                         b = Convert.ToUInt32(newValue[newValue.Length - 1].ToString());//converte o último algarismo
-                        answer = ApplyDeagglutination(a, b);//verifica se o valor é primo
+                        if (b == 1)//caso b seja igual a 1, deverá ser incrementado 10 para evitar falso positivo, quando chamado o método IsPrimeNumber
+                            answer = ApplyDeagglutination(a - 1, b + 10);//verifica se o valor é primo
+                        else//caso contrário
+                            answer = ApplyDeagglutination(a, b);//verifica se o valor é primo
+
                         if (!answer.Item1)//caso não seja primo
                             return answer;
                         else//caso conste como um possível número primo
@@ -68,13 +77,17 @@ namespace Deagglutination_Class
                             newValue = (Convert.ToUInt32(value) * 7).ToString();//novo valor, multiplicado por 7
                             a = Convert.ToUInt32(newValue.Substring(0, newValue.Length - 1));//converter os algarismo, até o penúltimo algarismo
                             b = Convert.ToUInt32(newValue[newValue.Length - 1].ToString());//converte o último algarismo
-                            answer = ApplyDeagglutination(a, b);//verifica se o valor é primo
+                            if (b == 1)//caso b seja igual a 1, deverá ser incrementado 10 para evitar falso positivo, quando chamado o método IsPrimeNumber
+                                answer = ApplyDeagglutination(a - 1, b + 10);//verifica se o valor é primo
+                            else//caso contrário
+                                answer = ApplyDeagglutination(a, b);//verifica se o valor é primo
+
                             return answer;
                         }
                     }
                     else//caso contrário
                     {
-                        if (b == 1)//verificação se o último algarismo é o número 1
+                        if (initialB == 1)//verificação se o último algarismo inicial é o número 3
                         {
                             newValue = (Convert.ToUInt32(value) * 3).ToString();//novo valor, multiplicado por 3
                             a = Convert.ToUInt32(newValue.Substring(0, newValue.Length - 1));//converter os algarismo, até o penúltimo algarismo
@@ -100,13 +113,16 @@ namespace Deagglutination_Class
 
         static Tuple<bool, string> ApplyDeagglutination(uint a, uint b)//método para verificar se trata de um numero primo de acordo com a hipótese da desaglutinação
         {
-            Console.WriteLine($"{a}   {b}");
+            uint initalA = a;//Consersvando argumento 'a' original
+            uint initialB = b;//Consersvando argumento 'b' original
+            //Console.WriteLine($"{a}   {b}");
             while (a != 0 && a % b != 0 && a > b)//caso 'a' seja diferente de 0, a divisão entre 'a' e 'b' *GERE* 'restos' *E* 'a', seja maior que 'b'. Faça:
             {
                 a--;//decremento de uma unidade
                 b += 10;//incremento de uma dezena de unidade
+                //Console.WriteLine($"{a}   {b}");
             }
-            if (a != 0 && a % b == 0 && a != b && b != 1)//caso 'a' seja diferente de 0, a divisão entre 'a' e 'b' *NÃO GERE* 'restos' e 'a' e 'b' sejam diferentes e 'b' seja diferente de 1
+            if (a != 0 && a % b == 0 && b != 1)//caso 'a' seja diferente de 0, a divisão entre 'a' e 'b' *NÃO GERE* 'restos' e 'b' seja diferente de 1
                 return new Tuple<bool, string>(false, b.ToString());//valor não é um número impar e possui divisor 'b'
             else
                 return new Tuple<bool, string>(true, null);//valor é um número impar e não retornará divisores, pois, por regra, seus divisores serão 1 e valor original do argumento 'a'.
--- Deagglutination_Hypothesis_PrimerNumberChecker/Program.cs
+++ Deagglutination_Hypothesis_PrimerNumberChecker/Program.cs
@@ -17,8 +17,20 @@ namespace Deagglutination_Hypothesis
             {
                 try
                 {
-                    Console.WriteLine($"Value: {i}\n    Answer: {Deagglutination.IsPrimeNumber(i)}");
-                    //Console.ReadKey();
+                    var answer = Deagglutination.IsPrimeNumber(i);
+                    if (answer.Item1)
+                    {
+                        if (/*true||*/IsReallyPrime(i)) //To confirm if the returned value from Deagglutination.IsPrimeNumber() method is a real prime number, remove the "true||" term and compile
+                        {
+                            Console.WriteLine($"Value: {i}\n    Answer: {answer}");
+                            //Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Value: {i}\n    Answer: (False, )");
+                            Console.ReadKey();
+                        }
+                    }
                 }
                 catch(Exception error)
                 {
@@ -27,6 +39,31 @@ namespace Deagglutination_Hypothesis
                 }
             }
             Console.ReadKey();
+        }
+
+        private static bool IsReallyPrime(uint value)
+        {
+            if (value == 0)
+                return true;
+            else if (value == 1)
+                return true;
+            else if (value % 2 == 0)
+                return true;
+            else if (value % 3 == 0)
+                return true;
+            else if (value % 5 == 0)
+                return true;
+            else if (value % 7 == 0)
+                return true;
+            else if (value % 9 == 0)
+                return true;
+            
+            for (uint i = value-1; i>2;i--)
+            {
+                if(value % i == 0)
+                    return false;
+            }
+            return true;            
         }
     }
 }
